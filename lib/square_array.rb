abc3a733aa9102a74db7515fb6ba27c8e73a52d4
Shane R <imspammails@gmail.com>
2021-01-22 11:24:52 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def square_array(array)
-  # your code here
+  new_array = [ ]
+  i = 0 
+  while i < array.length do
+    squared = array[i] ** 2
+    #binding.pry
+    new_array.push(squared)
+    i += 1 
+  end
+  new_array
 end
